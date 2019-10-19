8e285cb0f1e9839d23b517ab0bed6f044035ef79
Dhayllin <dhayllin@hotmail.com>
2019-10-19 01:29:32 -0300

    Imagens de bano publico(fakers )

--- database/factories/AntFactory.php
+++ database/factories/AntFactory.php
@@ -4,14 +4,15 @@
 
 use App\Ant;
 use Faker\Generator as Faker;
+use Bluemmb\Faker\PicsumPhotosProvider;
 
 $factory->define(Ant::class, function (Faker $faker) {
     return [
-        'photo_url' =>$faker->word.".png",
-        'latitude'  => $faker->latitude,
-        'longitude' => $faker->longitude,
-        'user_id'  => $faker->numberBetween($min = 1, $max = 20),
-        'created_at'     => $faker->dateTimeBetween($startDate = '-3 years', $endDate = 'now', $timezone = null), 
+        'photo_url'  => $faker->imageUrl(),
+        'latitude'   => $faker->latitude,
+        'longitude'  => $faker->longitude,
+        'user_id'    =>  $faker->numberBetween($min = 1, $max = 20),
+        'created_at' => $faker->dateTimeBetween($startDate = '-3 years', $endDate = 'now', $timezone = null), 
     ];
 });
 
